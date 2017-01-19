85d1f50b1baffd42afcacd94ba9c4f698a61f343
wppark <wppark.pio@gmail.com>
2017-01-19 17:37:04 +0900

    Scala {pre/in/post}fix operator example

---
+++ src/main/scala-2.12/PrefixOperator.scala
@@ -0,0 +1,23 @@
+package org.lenscloth.scala.examples
+
+object PrefixOperator {
+  case class Number(x: Int) {
+    // Infix operator
+    def +(that: Number) = Number(x + that.x)
+
+    // Postfix operator
+    def -- = Number(x - 1)
+
+    // prefix operator
+    def unary_! = Number(-x)
+
+    override def toString = x.toString
+  }
+
+  def main(args: Array[String]) = {
+    println(s"1 + 1 = ${Number(1) + Number(1)}")
+    println(s"1 -- = ${Number(1) --}")
+    println(s"!1 = ${!Number(1)}")
+  }
+}
+
