35f7d4c90dc1236cfce2f7069f8fc595cee34b37
wppark <wppark.pio@gmail.com>
2017-01-19 19:24:39 +0900

    Change example code

--- src/main/scala-2.12/org/lenscloth/scala/examples/Associative.scala
+++ src/main/scala-2.12/org/lenscloth/scala/examples/Associative.scala
@@ -4,19 +4,19 @@ object Associative {
   // Left Associative
   // caller method argument
   case class NumberLeft(x: Int) {
-    def /(v: Int) = NumberLeft(x / v)
+    def /(that: NumberLeft) = NumberLeft(x / that.x)
     override def toString = x.toString
   }
 
   // Right Associative
   // argument method caller
   case class NumberRight(x: Int) {
-    def /:(v: Int) = NumberRight(v / x)
+    def /:(that: NumberRight) = NumberRight(that.x / x)
     override def toString = x.toString
   }
 
   def main(args: Array[String]) = {
-    println(s"50 / (25 / 5) == ${50 /: 25 /: NumberRight(5)}")
-    println(s"(50 / 25) / 5 == ${NumberLeft(50) / 25 / 5}")
+    println(s"50 / (25 / 5) == ${NumberRight(50) /: NumberRight(25) /: NumberRight(5)}")
+    println(s"(50 / 25) / 5 == ${NumberLeft(50) / NumberLeft(25) / NumberLeft(5)}")
   }
 }
